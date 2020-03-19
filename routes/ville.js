e554598aee56354ef1cc7947fb25f9ac3fe0689e
SoKaii <t.martin92500@hotmail.Fr>
2020-03-19 15:26:11 +0100

    Fix bug and use the ville.hjs

--- routes/ville.js
+++ routes/ville.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send("Ville: " + req.body.nom_ville);
-});
-
 router.post('/', function(req, res, next) {
-    res.send("Ville: " + req.body.nom_ville);
+    res.render("ville", {nom_ville: req.body.nom_ville});
   });
 
 module.exports = router;
